6469c912ea2db82dc05b1aee6bd7e0569864f7e8
ahrsoftware <artsresearchsoftware@gmail.com>
2021-08-21 15:44:19 +0100

    first attempt at process list

--- django/datadriven/migrations/0002_data_initial.py
+++ django/datadriven/migrations/0002_data_initial.py
@@ -11,57 +11,71 @@ def insert_services(apps, schema_editor):
         {
             'name': 'Advice',
             'fontawesome_icon': 'fas fa-comments',
-            'description': "We offer free advice about lots of things \
-                            and more stuff",
+            'description': "We offer free, no-obligation advice sessions \
+                            where we discuss how software and data can \
+                            play a vital role in your research project.",
             'order': 1
         },
         {
             'name': 'Websites',
             'fontawesome_icon': 'fas fa-desktop',
-            'description': "x \
-                            x",
+            'description': "We build bespoke, advanced websites to \
+                            collect, store, and present your research data. \
+                            We can design, develop, host, and maintain your \
+                            website throughout the lifetime of your project.",
             'order': 2
         },
         {
             'name': 'Mobile Apps',
             'fontawesome_icon': 'fas fa-mobile-alt',
-            'description': "x \
-                            x",
+            'description': "We develop beautiful mobile apps for both \
+                            Android and iOS devices to deliver an engaging and \
+                            impactful experience for your research participants.",
             'order': 3
         },
         {
             'name': 'Data Collection',
             'fontawesome_icon': 'fas fa-keyboard',
-            'description': "x \
-                            x",
+            'description': "We use software to improve the quantity and \
+                            quality of the research data you collect. \
+                            We can scrape large amounts of data from the web \
+                            and gather high-quality responses from custom web surveys.",
             'order': 4
         },
         {
             'name': 'Data Storage',
             'fontawesome_icon': 'fas fa-database',
-            'description': "x \
-                            x",
+            'description': "We can determine the most appropriate way to \
+                            store your data, whether it's as a SQL database \
+                            or in a format like XML, JSON, or CSV. \
+                            We'll help you design an efficient data \
+                            structure and help you store your data securely.",
             'order': 5
         },
         {
             'name': 'Data Visualisations',
             'fontawesome_icon': 'fas fa-chart-pie',
-            'description': "x \
-                            x",
+            'description': "Data visualisations are an extremely engaging \
+                            way to read, understand, and share your data. \
+                            We can design interactive visualisations and \
+                            embed them in a website or mobile app.",
             'order': 6
         },
         {
             'name': 'Data Quality',
             'fontawesome_icon': 'fas fa-project-diagram',
-            'description': "x \
-                            x",
+            'description': "We can write software to automatically detect \
+                            inconsistencies and errors in your data, leading to \
+                            higher quality data and more accurate research outputs.",
             'order': 7
         },
         {
             'name': 'Project Branding',
             'fontawesome_icon': 'fas fa-palette',
-            'description': "x \
-                            x",
+            'description': "We can design unique and attractive branding \
+                            for your research project and apply this to your \
+                            project website to make a consistent and professional \
+                            impression on your audience.",
             'order': 8
         },
     ]
